516d12b079f7c6df7ce036b77393d35c2dad6a6a
John Takeuchi <hirokiht@gmail.com>
2014-09-26 13:07:07 +0800

    Fixed error reporting to report if fs_open returning negative values.

--- src/shell.c
+++ src/shell.c
@@ -67,8 +67,7 @@ int filedump(const char *filename){
 	char buf[128];
 
 	int fd=fs_open(filename, 0, O_RDONLY);
-
-	if(fd==OPENFAIL)
+	if(fd==OPENFAIL || fd < 0)
 		return 0;
 
 	fio_printf(1, "\r\n");
